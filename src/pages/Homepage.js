1298c10e9b2a12166dc13c5292954c4c9c9764f6
Amaya Jenkins <amayajenkins66@gmail.com>
2020-10-16 21:13:11 -0400

    changed homepage back

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,29 +1,10 @@
 import React from "react"
-import AdamsNavbar from "../components/AdamsNavbar"
-import { Container } from "semantic-ui-react"
 
 function Homepage() {
 
 
     return (
-        <div style={{ width: "100%" }}>
-            <AdamsNavbar></AdamsNavbar>
-            <Container>
-            <iframe 
-                src="https://instagram.com/p/BpKjlo-B4uI/embed"
-                frameBorder="0"
-                allowFullScreen
-                scrolling="no"
-                allowTransparency
-                width="320"
-                height="320"
-            ></iframe>
-        </Container>
-        </div>
-        
-
-        
-
+        <h1>homepage</h1>
     )
 
 
