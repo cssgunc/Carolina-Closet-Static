f5c7d2b935d1616a4ce46d3465e04287631fcc88
Amaya Jenkins <amayajenkins66@gmail.com>
2020-10-16 20:01:49 -0400

    updated langing page and added instagram template to homepage

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,10 +1,29 @@
 import React from "react"
+import AdamsNavbar from "../components/AdamsNavbar"
+import { Container } from "semantic-ui-react"
 
 function Homepage() {
 
 
     return (
-        <h1> homepage</h1>
+        <div style={{ width: "100%" }}>
+            <AdamsNavbar></AdamsNavbar>
+            <Container>
+            <iframe 
+                src="https://instagram.com/p/BpKjlo-B4uI/embed"
+                frameBorder="0"
+                allowFullScreen
+                scrolling="no"
+                allowTransparency
+                width="320"
+                height="320"
+            ></iframe>
+        </Container>
+        </div>
+        
+
+        
+
     )
 
 
