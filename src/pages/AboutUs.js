7f8fa8d4f8187279fba3021db3eaf8b2ef0d9371
Adam Winek <adamwinek@gmail.com>
2020-10-25 11:35:11 -0600

    Configuration test

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -34,6 +34,17 @@ function AboutUs() {
 
     return (
         <Ref innerRef={stickyRef}>
+            <script>
+                if (window.netlifyIdentity) {
+                    window.netlifyIdentity.on("init", user => {
+                        if (!user) {
+                            window.netlifyIdentity.on("login", () => {
+                                document.location.href = "/admin/";
+                            });
+                        }
+                    });
+  }
+</script>
 
             <div style={{ width: "100%" }} >
                 <Sticky ref={stickyRef}>
