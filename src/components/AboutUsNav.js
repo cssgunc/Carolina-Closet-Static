c5de770ac53f7eebdb44b15cd3c367ec17701396
Amaya Jenkins <amayajenkins66@gmail.com>
2020-10-30 12:20:37 -0400

    improved landing page css

--- src/components/AboutUsNav.js
+++ src/components/AboutUsNav.js
@@ -1,39 +1,75 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { isMobile } from 'react-device-detect';
 import { Container, Header, Rail } from 'semantic-ui-react'
 import "semantic-ui-less/semantic.less";
+import { doc } from 'prettier';
+import AboutUsIndicator from "./AboutUsIndicator";
 
 
 
+function AboutUsNav(props) {
+    //scroll to element function
+    const scrollToRef = (ref) => {
+        document.body.scrollTo(
+            {
+                top: ref.current.offsetTop,
+                left: 0,
+                behavior: 'smooth'
+            });
+    }
+    const [scrollHeight, setScrollHeight] = useState(0);
+    useEffect(() => {
+        document.body.addEventListener('scroll', () => handleScroll());
+        return (
+            document.body.removeEventListener('scroll', () => handleScroll())
+        )
+    })
 
-function AboutUsNav() {
+    let handleScroll = (() => {
+        setScrollHeight(document.body.scrollTop);
+    });
 
 
+    console.log(scrollHeight)
     return (
-        <Rail size='massive' position='left' >
-            <div className="aboutUsNav">
-                <h2 className="aboutUsNavItem">
-                    Home
-                </h2>
-                <h2 className="aboutUsNavItem">
-                    Donations
-                </h2>
-                <h2 className="aboutUsNavItem">
-                    Vision & Mission
-                </h2>
-                <h2 className="aboutUsNavItem">
-                    Partners
-                </h2>
-                <h2 className="aboutUsNavItem">
-                    Rentals
-                </h2>
+        <div className="aboutUsNav">
+            <div >
+                <div >
+                    <AboutUsIndicator scroll={scrollHeight} height={props.arrRefs[0]} />
+                    <Header size="huge" className="aboutUsNavItem" onClick={() => scrollToRef(props.arrRefs[0])}>
+                        About
+                    </Header>
+                </div>
+                <div>
+                    <AboutUsIndicator scroll={scrollHeight} height={props.arrRefs[1]} />
+                    <Header size="huge" className="aboutUsNavItem" onClick={() => scrollToRef(props.arrRefs[1])}>
+                        Donations
+                    </Header>
+                </div>
+                <div>
+                    <AboutUsIndicator scroll={scrollHeight} height={props.arrRefs[2]} />
+                    <Header size="huge" className="aboutUsNavItem" onClick={() => scrollToRef(props.arrRefs[2])}>
+                        Vision & Mission
+                    </Header>
+                </div>
+                <div>
+                    <AboutUsIndicator scroll={scrollHeight} height={props.arrRefs[3]} />
+                    <Header size="huge" className="aboutUsNavItem" onClick={() => scrollToRef(props.arrRefs[3])}>
+                        Partners
+                </Header>
+                </div>
+                <div>
+                    <AboutUsIndicator scroll={scrollHeight} height={props.arrRefs[4]} />
+                    <Header size="huge" className="aboutUsNavItem" onClick={() => scrollToRef(props.arrRefs[4])}>
+                        Rentals
+                </Header>
+                </div>
+
 
             </div>
 
-
-
-
-        </Rail >);
+        </div >
+    );
 
 
 
