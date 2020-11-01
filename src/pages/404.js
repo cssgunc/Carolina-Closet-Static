99439b87b1c69a655c1529af5654865bb99fea97
Adam Winek <adamwinek@gmail.com>
2020-11-01 16:19:04 -0500

    getting rid of template material

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,29 +1,13 @@
 import React from "react"
-import { graphql } from "gatsby"
 
-import Layout from "../components/layout"
-import SEO from "../components/seo"
 
-const NotFoundPage = ({ data, location }) => {
-  const siteTitle = data.site.siteMetadata.title
+const NotFoundPage = () => {
 
   return (
-    <Layout location={location} title={siteTitle}>
-      <SEO title="404: Not Found" />
-      <h1>Not Found</h1>
-      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-    </Layout>
+
+    <h1>The shit yyou looking for aint here</h1>
+
   )
 }
 
-export default NotFoundPage
 
-export const pageQuery = graphql`
-  query {
-    site {
-      siteMetadata {
-        title
-      }
-    }
-  }
-`
