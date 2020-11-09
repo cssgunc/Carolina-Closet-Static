bdb6e896ae10c4dd87112943cf660df7ebedbafe
Adam Winek <adamwinek@gmail.com>
2020-11-08 19:02:44 -0500

    changes for landing page

--- src/pages/Landingpage.js
+++ src/pages/Landingpage.js
@@ -6,59 +6,59 @@ import { graphql, useStaticQuery } from "gatsby"
 
 function LandingPage() {
 
-    const data = useStaticQuery(graphql`
-    query MyLandingQuery {
-        allMarkdownRemark(filter: {fields: {slug: {eq: "/LandingPage/landing-page/"}}}) {
-          edges {
-            node {
-              frontmatter {
-                title
-                SubHeader
-                image
-              }
-            }
-          }
-        }
-      }
-    `)
-  const { allMarkdownRemark } = data // data.markdownRemark holds your post data
-  const { frontmatter } = allMarkdownRemark.edges[0].node
+  // const data = useStaticQuery(graphql`
+  //   query MyLandingQuery {
+  //       allMarkdownRemark(filter: {fields: {slug: {eq: "/LandingPage/landing-page/"}}}) {
+  //         edges {
+  //           node {
+  //             frontmatter {
+  //               title
+  //               SubHeader
+  //               landingPageimage
+  //             }
+  //           }
+  //         }
+  //       }
+  //     }
+  //   `)
+  // const { allMarkdownRemark } = data // data.markdownRemark holds your post data
+  // const { frontmatter } = allMarkdownRemark.edges[0].node
 
-    return (
-       
-            <div style={{ width: "100%" }}>
-                    <Container id="landingpageContent" className="landingpageContentContainer" style={{ backgroundImage: `url(${frontmatter.image})`}}>
-                        <Container className="landingpageContainer">
-                            <Header as="h1" className="landingpageName">{frontmatter.title}</Header>
-                            <Header as="h3" className="landingpageSlogan">{frontmatter.SubHeader}</Header>
+  return (
 
-                            <div className="ui centered cards">
-                                <div className="landingpage ui card">
-                                    <div className="landingpage card content">
-                                        <div className="landingpage description a">Are you a Student?</div>
-                                        <button className="landingpage ui button a">
-                                            Browse the inventory
+    <div style={{ width: "100%" }}>
+      <Container id="landingpageContent" className="landingpageContentContainer" style={{ backgroundImage: `url(${frontmatter.image})` }}>
+        <Container className="landingpageContainer">
+          <Header as="h1" className="landingpageName">{frontmatter.title}</Header>
+          <Header as="h3" className="landingpageSlogan">{frontmatter.SubHeader}</Header>
+
+          <div className="ui centered cards">
+            <div className="landingpage ui card">
+              <div className="landingpage card content">
+                <div className="landingpage description a">Are you a Student?</div>
+                <button className="landingpage ui button a">
+                  Browse the inventory
                                         </button>
-                                    </div>
-                                </div>
-                                <div className="landingpage ui card">
-                                    <div className="landingpage card content">
-                                        <div className="landingpage description b">Volunteer or Donor?</div>
-                                        <button className="landingpage ui button b">
-                                            Learn More
+              </div>
+            </div>
+            <div className="landingpage ui card">
+              <div className="landingpage card content">
+                <div className="landingpage description b">Volunteer or Donor?</div>
+                <button className="landingpage ui button b">
+                  Learn More
 
                                         </button>
-                            </div>
-                        </div>
-                    </div>
+              </div>
+            </div>
+          </div>
 
-                </Container>
-            </Container>
+        </Container>
+      </Container>
 
 
-        </div>
+    </div>
 
-    )
+  )
 
 
 } export default LandingPage
